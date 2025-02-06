cb74a0aa02e4df80b9e3a52e8fb63d5dbaca4f0a
shanky17 <shashaank.soni@gmail.com>
2025-02-06 22:20:00 +0530

    Added docker compose

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -88,10 +88,7 @@ DB_HOST = os.environ.get('POSTGRES_HOST')
 DB_PORT = os.environ.get('POSTGRES_PORT')
 DB_IS_AVAIL = all([DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST, DB_PORT])
 
-POSTGRES_READY = str(os.environ.get('POSTGRES_READY')) == '1'
-
-
-if DB_IS_AVAIL and POSTGRES_READY:
+if DB_IS_AVAIL:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
